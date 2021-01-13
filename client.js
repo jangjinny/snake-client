c334ceaf2555ff5d0dcda801055efe412ef38bdc
Jinny Jang <jangjinny21@gmail.com>
2021-01-13 21:18:17 +0000

    added connect event and a name to be sent to the server

--- client.js
+++ client.js
@@ -1,20 +1,26 @@
 const net = require('net');
+
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
+  const conn = net.createConnection({ 
+    host: 'localhost',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8');
+  conn.setEncoding('utf8'); 
 
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
 
-  return conn;
-};
+  conn.on('connect', () => {
+    console.log('Server is connected');
+    conn.write('Name: JJ');
+  });
 
-module.exports = connect;
+  return conn;
+}
+
+module.exports = { connect }; 
