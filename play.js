cf70f64a2c7d023d00e70dfa8102dc2253b40988
Jinny Jang <jangjinny21@gmail.com>
2021-01-14 01:31:30 +0000

    sent connection object from connect into setupInput

--- play.js
+++ play.js
@@ -1,6 +1,10 @@
 /**
  * Establishes connection with the game server
  */
-const { connect } = require('./client')
-console.log('Connecting...');
-connect();
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+let connection; 
+
+console.log('Connecting ...');
+
+setupInput(connect());
